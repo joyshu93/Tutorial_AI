4ec57b046dcee99d75434e7eed53161ba411d6a1
joyshu <joyshu@pineone.com>
2024-01-10 17:04:02 +0900

    ver 0.0.2

--- app.py
+++ app.py
@@ -1,38 +1,43 @@
 import tensorflow as tf
-import numpy as np
-
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+import numpy as np
 
-# 가상의 텍스트 데이터 생성
-corpus = ["I love programming with TensorFlow.", "TensorFlow is a powerful tool.", "Deep learning is fascinating."]
+# 간단한 대화 데이터
+conversations = [
+    ("안녕", "안녕하세요"),
+    ("뭐해", "일하고 있어요."),
+    ("잘가", "안녕히 가세요!")
+]
+
+# 입력과 출력 문장 분리
+questions, answers = zip(*conversations)
 
 # 토큰화
 tokenizer = Tokenizer()
-tokenizer.fit_on_texts(corpus)
+tokenizer.fit_on_texts(questions + answers)
 total_words = len(tokenizer.word_index) + 1
 
 # 문장을 시퀀스로 변환
-input_sequences = []
-for line in corpus:
-    token_list = tokenizer.texts_to_sequences([line])[0]
-    for i in range(1, len(token_list)):
-        n_gram_sequence = token_list[:i+1]
-        input_sequences.append(n_gram_sequence)
+input_sequences = tokenizer.texts_to_sequences(questions)
+output_sequences = tokenizer.texts_to_sequences(answers)
 
 # 패딩
-max_sequence_length = max([len(seq) for seq in input_sequences])
-input_sequences = pad_sequences(input_sequences, maxlen=max_sequence_length, padding='pre')
+max_sequence_length = max(max(len(seq) for seq in input_sequences), max(len(seq) for seq in output_sequences))
+input_sequences = pad_sequences(input_sequences, maxlen=max_sequence_length, padding='post')
+output_sequences = pad_sequences(output_sequences, maxlen=max_sequence_length, padding='post')
 
-# 입력과 레이블 생성
-X, y = input_sequences[:,:-1], input_sequences[:,-1]
-y = tf.keras.utils.to_categorical(y, num_classes=total_words)
+# 입력 및 출력 데이터 준비
+X = input_sequences
+y = tf.keras.utils.to_categorical(output_sequences, num_classes=total_words)
 
-# 모델 정의
+# Seq2Seq 모델 정의
 model = tf.keras.Sequential([
-    tf.keras.layers.Embedding(total_words, 64, input_length=max_sequence_length-1),
+    tf.keras.layers.Embedding(total_words, 64, input_length=max_sequence_length),
     tf.keras.layers.LSTM(100),
-    tf.keras.layers.Dense(total_words, activation='softmax')
+    tf.keras.layers.RepeatVector(max_sequence_length),
+    tf.keras.layers.LSTM(100, return_sequences=True),
+    tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(total_words, activation='softmax'))
 ])
 
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
@@ -40,22 +45,16 @@ model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accur
 # 모델 학습
 model.fit(X, y, epochs=100, verbose=1)
 
-# 텍스트 생성 함수
-def generate_text(seed_text, next_words, model, max_sequence_length):
-    for _ in range(next_words):
-        token_list = tokenizer.texts_to_sequences([seed_text])[0]
-        token_list = pad_sequences([token_list], maxlen=max_sequence_length-1, padding='pre')
-        predicted_probabilities = model.predict(token_list, verbose=0)[0]
-        predicted = np.argmax(predicted_probabilities)
-        output_word = ""
-        for word, index in tokenizer.word_index.items():
-            if index == predicted:
-                output_word = word
-                break
-        seed_text += " " + output_word
-    return seed_text
+# 대화 생성 함수
+def generate_response(user_input):
+    input_seq = tokenizer.texts_to_sequences([user_input])
+    padded_input_seq = pad_sequences(input_seq, maxlen=max_sequence_length, padding='post')
+    predicted_probabilities = model.predict(padded_input_seq, verbose=0)[0]
+    predicted_sequence = [np.argmax(probabilities) for probabilities in predicted_probabilities]
+    response = tokenizer.sequences_to_texts([predicted_sequence])[0]
+    return response
 
-
-# 텍스트 생성
-generated_text = generate_text("I love", 5, model, max_sequence_length)
-print(generated_text)
+# 대화 테스트
+user_input = "안녕"
+response = generate_response(user_input)
+print("챗봇:", response)
